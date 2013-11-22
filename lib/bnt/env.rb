bf168d62ee003aca3237d0909a4f6dcf4f28bf34
Rayko <rayko.drg@gmail.com>
2013-11-22 14:24:35 -0300

    Package is now being copied to a temp dir before compilation

--- lib/bnt/env.rb
+++ lib/bnt/env.rb
@@ -1,18 +1,22 @@
 class Cage
-  
+
   def self.path
     error_msg = <<-EOS "\n\nEmpty cage path please ensure that CAGE_PATH env var is available 'export CAGE_PATH=/home/user/bosh-cages'"
 EOS
     raise error_msg if ENV['CAGE_PATH'].nil?
     ENV['CAGE_PATH']
   end
-  
+
   def self.packages_path
     "/var/vcap/packages"
   end
-  
+
   def self.package_path(pkg)
     "#{path}#{packages_path}/#{pkg}"
+  end
+
+  def self.tmp_dir(pkg)
+    "#{path}/tmp/#{pkg}"
   end
 end
 
